0ea027247f2fcba40b9d39c31ed4bb27420c37de
bhagyashree-2024 <officialwork20254@gmail.com>
2024-12-23 17:05:14 +0530

    testing

---
+++ firstDecisionService/.decisionservice/test/java/firstDecisionServiceDecisionService/Test.java
@@ -0,0 +1,9 @@
+package firstDecisionServiceDecisionService;
+
+import junit.framework.TestCase;
+
+public class Test extends TestCase {
+	public void testPersonWithMiddleName() {
+		assertEquals(true, true);
+	}
+}
